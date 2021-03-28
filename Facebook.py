e99be568a53568cc60be2b452909d32df5159e0d
roussme <81525007+roussme@users.noreply.github.com>
2021-03-28 13:49:42 -0400

    add existing file

---
+++ Facebook.py
@@ -0,0 +1,7 @@
+userName = input("Hello! Please enter your facebook username! ")
+userPassword = input("Hello! Please enter your facebook password! ")
+
+if(userName.lower() == "madeline" and userPassword == "IlikeCSF534!@"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
